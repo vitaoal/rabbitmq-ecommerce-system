83eb1ccc0f2bbd5da73482a722cda16172b07e0e
Huan Thomas <59237379+huantssousa@users.noreply.github.com>
2024-10-28 15:55:42 -0300

    Add files via upload

--- supplier.py
+++ supplier.py
@@ -1,37 +1,55 @@
 import pika
 import sys
+from functools import partial
 from asym_cript import *
 
-def initConnection(host = 'localhost'):
+def initConnection(host='localhost'):
     connection = pika.BlockingConnection(pika.ConnectionParameters(host))
     channel = connection.channel()
-    channel.queue_declare(queue='order_queue')
     return channel
 
-def callback(ch, method, properties, body):
+def callback(ch, method, properties, body, private, channel2):
     body = body.decode()
-    print(f" [x] Received {body}")
     message, signature = body.split('|', 1)
-
     signature = bytes.fromhex(signature)
     public_key = load_public_key(r'keys\client\public_key.pem')
-
-    # Verifica a assinatura da mensagem com a chave pública do fornecedor
     is_valid = verify(signature, message.encode(), public_key)
     print(f" [x] Received {message} - Assinatura válida: {is_valid}")
 
+    if is_valid == True :
+        
+        print("\n === ENVIO DE MERCADORIA === ")
+
+        message2 = ' '.join(sys.argv[1:]) or "Jaqueta Nike"
+        signature = sign(message2.encode(), private).hex()
+        message1 = f"{message2}|{signature}"
+
+        channel2.basic_publish(
+                exchange='',
+                routing_key='task_queue',
+                body=message1
+            )
+        print(f" [x] Sent {message2}")
+
 def main():
-    print("Iniciando fornecedor...")
     channel = initConnection()
 
+    # Configura a exchange 'order_type' do tipo 'topic'
     channel.exchange_declare(exchange='order_type', exchange_type='topic')
-    queue_name = 'order_queue'
+    channel2 = initConnection()
+    channel2.queue_declare(queue='task_queue', durable=True)
+    private = load_private_key(r'keys\supplier_1\private_key.pem')
 
-    # Liga a fila 'order_queue' à exchange 'order_type' com a chave '*' recebendo todas as msgs
-    channel.queue_bind(exchange='order_type', queue=queue_name, routing_key='order.*')
+    
+    # Nome único da fila para 'order.clothes'
+    queue_name = 'order_queue_clothes'
+    channel.queue_declare(queue=queue_name, exclusive=True)
+    channel.queue_bind(exchange='order_type', queue=queue_name, routing_key='order.clothes')
 
-    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+    # Consumir mensagens específicas do tópico 'order.clothes'
+    channel.basic_consume(queue=queue_name, on_message_callback= partial(callback, private = private, channel2 = channel2), auto_ack=True)
 
+    print("Aguardando mensagens do tópico 'order.clothes'...")
     channel.start_consuming()
 
 if __name__ == "__main__":
